50f26adceb48b4c3ba1a51dbfc4d86073a7d134c
heecheol153 <heecheol153@gmail.com>
2021-10-02 21:12:51 +0900

    #5.1 Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000);
+getClock(); //없으면00:00:00부터 시작함
+setInterval(getClock, 1000);
