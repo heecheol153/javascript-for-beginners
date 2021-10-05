cc8ca8d5a8d472dc78005c2a38bb1205099fa002
heecheol153 <heecheol153@gmail.com>
2021-10-05 10:45:23 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,17 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "4eb11d4e80ed47f574f4b7bf3c437362";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((date) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
   console.log("You live in", lat, lng);
 }
 function onGeoError() {
